1744a2e05b5b4211f65503c4bd41d9b50db5aacf
Andreas Nordahl <andnorda@gmail.com>
2015-07-26 21:55:08 +0200

    backbone validation test

--- test.js
+++ test.js
@@ -1,11 +1,25 @@
-var Account = require('backbone').Model;
+var _ = require('underscore');
+var Backbone = require('backbone');
+require('backbone-validation')
+_.extend(Backbone.Model.prototype, Backbone.Validation.mixin);
+
+var Account = Backbone.Model.extend({
+    validation: {
+        name: {
+            required: true,
+            msg: 'name, plz'
+        }
+    }
+});
 
 describe('Account', function() {
-    it('fires change event', function(done) {
+    it('name is not valid when empty', function() {
         var account = new Account();
-        account.on('change', function() {
-            done();
-        });
+        expect(account.validate()).toEqual({name: 'name, plz'});
+    });
+    it('name is valid when fanto', function() {
+        var account = new Account();
         account.set('name', 'fanto');
+        expect(account.validate()).toBeUndefined();
     });
 });
