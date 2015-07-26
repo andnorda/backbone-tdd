36e26fa923a4a4c0651233e08bb3d4d06af53ba1
Andreas Nordahl <andnorda@gmail.com>
2015-07-26 22:40:16 +0200

    backbone stickit test

--- accountView.spec.js
+++ accountView.spec.js
@@ -1,8 +1,14 @@
 var Backbone = require('backbone');
+require('backbone.stickit');
 
 var AccountView = Backbone.View.extend({
+    model: new Backbone.Model,
+    bindings: {
+        '#name': 'name'
+    },
     render: function() {
         this.$el.html('<input id="name">');
+        this.stickit();
     }
 });
 
@@ -10,6 +16,12 @@ describe('Account View', function() {
     it('has a name field', function() {
         var accountView = new AccountView();
         accountView.render();
-        expect(accountView.$('input#name').length).toEqual(1);
+        expect(accountView.$('#name').length).toEqual(1);
+    });
+    it('model change updates dom', function() {
+        var accountView = new AccountView();
+        accountView.render();
+        accountView.model.set('name', 'fanto');
+        expect(accountView.$('#name').val()).toEqual('fanto');
     });
 });
