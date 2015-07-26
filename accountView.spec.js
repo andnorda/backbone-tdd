d6437b173a88ded0b7632cd0758c135c9047892a
Andreas Nordahl <andnorda@gmail.com>
2015-07-26 22:29:08 +0200

    simple view test

--- accountView.spec.js
+++ accountView.spec.js
@@ -1,9 +1,15 @@
 var Backbone = require('backbone');
 
-var AccountView = Backbone.View;
+var AccountView = Backbone.View.extend({
+    render: function() {
+        this.$el.html('<input id="name">');
+    }
+});
 
 describe('Account View', function() {
     it('has a name field', function() {
         var accountView = new AccountView();
+        accountView.render();
+        expect(accountView.$('input#name').length).toEqual(1);
     });
 });
